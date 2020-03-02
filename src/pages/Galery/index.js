dd9d493f196b9e4f022b440c2a73156d4a67dc00
Matheus Pavanelli <mspavanelli@gmail.com>
2020-03-02 00:18:42 -0300

    feat(galery): add basic page styles

--- src/pages/Galery/index.js
+++ src/pages/Galery/index.js
@@ -1,7 +1,19 @@
 import React from 'react'
 
-import { Container } from './styles'
+import FoundationII from '../../assets/images/fundacao-imperio.jpg'
+import Foundation from '../../assets/images/fundacao.jpg'
+import FoundationIII from '../../assets/images/segunda-fundacao.jpg'
+import { Container, Pictures } from './styles'
 
-const Galery = () => <Container>Galery</Container>
+const Galery = () => (
+  <Container>
+    <h1>Galery</h1>
+    <Pictures>
+      <img src={Foundation} alt="Foundation" />
+      <img src={FoundationII} alt="Foundation II" />
+      <img src={FoundationIII} alt="Foundation III" />
+    </Pictures>
+  </Container>
+)
 
 export default Galery
--- src/pages/Galery/styles.js
+++ src/pages/Galery/styles.js
@@ -1,3 +1,21 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  max-width: 90%;
+  margin: auto;
+
+  h1 {
+    font-weight: normal;
+    margin: 20px 0;
+  }
+`
+
+export const Pictures = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: repeat(4, 1fr);
+
+  img {
+    max-width: 100%;
+  }
+`
